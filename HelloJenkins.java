171898b0e5fe63edf757028b787d49af4fe0c5d0
rashmi <kurmirashmi27@gmail.com>
2026-08-13 08:55:02 +0530

    Update HelloJenkins.java

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,1 +1,5 @@
 public class HelloJenkins { public static void main(String[] args) { System.out.println("hi i am rashmi kurmi "); } } 
+public class HelloJenkins { public static void main(String[] args) { System.out.println("hello world "); } } 
+
+public class HelloJenkins { public static void main(String[] args) { System.out.println("BSCIT "); } } 
+
